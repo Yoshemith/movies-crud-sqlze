59ec52c234171ca171f545c5a1ee9d77fcf29d25
Yoshemith <cv3227@innovaccion.mx>
2021-10-14 19:39:40 -0500

    update repo!

--- src/app.js
+++ src/app.js
@@ -1,53 +1,25 @@
-// ************ Require's ************
-const createError = require('http-errors');
-const cookieParser = require('cookie-parser');
 const express = require('express');
-const logger = require('morgan');
 const path = require('path');
-const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
 
-// ************ express() - (don't touch) ************
+
+const indexRouter = require('./routes/index');
+
+const moviesRoutes = require('./routes/moviesRoutes');
+const genresRoutes = require('./routes/genresRoutes');
 const app = express();
 
-// ************ Middlewares - (don't touch) ************
-app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
-app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(cookieParser());
-app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
-
-// ************ Template Engine - (don't touch) ************
+// view engine setup
+app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+//URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
+app.use(express.urlencoded({ extended: false }));
 
 
+app.use('/', indexRouter);
+app.use(moviesRoutes);
+app.use(genresRoutes);
 
-// ************ WRITE YOUR CODE FROM HERE ************
-// ************ Route System require and use() ************
-const mainRouter = require('./routes/main'); // Rutas main
-const productsRouter = require('./routes/products'); // Rutas /products
-
-app.use('/', mainRouter);
-app.use('/products', productsRouter);
-
-
-
-// ************ DON'T TOUCH FROM HERE ************
-// ************ catch 404 and forward to error handler ************
-app.use((req, res, next) => next(createError(404)));
-
-// ************ error handler ************
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.path = req.path;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// ************ exports app - dont'touch ************
-module.exports = app;
+app.listen('3001', () => console.log('Servidor corriendo en el puerto 3001'));
